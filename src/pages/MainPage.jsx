723ff34398824e8da4574d5f5bba83b2808f0bb6
Remigiusz Woźniak <remek778790@gmail.com>
2022-11-08 14:26:14 +0100

    styled main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import adventureJpg from '../assets/adventure.jpg';
+import arenaJpg from '../assets/arena.jpg';
+import shopJpg from '../assets/shop.png';
 
 const MainPage = () => {
-  return <h1>Mainpage</h1>;
+  const importantLinks = [
+    { title: 'Adventure', img: adventureJpg },
+    { title: 'Arena', img: arenaJpg },
+    { title: 'Shop', img: shopJpg },
+  ];
+  return (
+    <>
+      <div className="bg-danger text-center mt-5">
+        <h1 className="py-1">Welcome to the city far traveler</h1>
+      </div>
+      <div className="mainPage_card-container mx-auto mt-5 d-flex align-items-center justify-content-center">
+        {importantLinks.map((link, id) => (
+          <div
+            key={id}
+            id={id}
+            className="card h-100 mx-3 mainPage_card border-0"
+          >
+            <NavLink to={`/${link.title}`} className="w-100">
+              <div className="card-img-top bg-black">
+                <img src={link.img} alt="frame" className="w-100 rounded-top" />
+              </div>
+              <div className="card-body">{link.title}</div>
+            </NavLink>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default MainPage;
